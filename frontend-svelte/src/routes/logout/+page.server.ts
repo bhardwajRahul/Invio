6b2e5002aa14d65770963027ecccb025b2f88f91
CodingKitten <codingkitten@disroot.org>
2026-03-31 11:59:25 +0200

    Delete frontend-svelte directory

--- frontend-svelte/src/routes/logout/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
-import { SESSION_COOKIE } from "$lib/backend";
-
-export const load: PageServerLoad = async ({ cookies }) => {
-  cookies.delete(SESSION_COOKIE, { path: "/" });
-  throw redirect(303, "/login");
-};
--- frontend-svelte/src/routes/public/invoices/[token]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { error, redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
-
-export const load: PageServerLoad = async ({ params }) => {
-    // Just pass the token to the page so it can construct the iframe source
-    // No backend communication here, backend serves the actual HTML via iframe!
-    return { token: params.token };
-};
